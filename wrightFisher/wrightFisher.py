0a6f78ef350f129d98bd5454c1131175964c8b72
Maya <mmastro5@jhu.edu>
2023-09-08 14:43:29 -0400

    Code with all the various graphs attached, the three exercises are all in that code

--- wrightFisher/wrightFisher.py
+++ wrightFisher/wrightFisher.py
@@ -21,6 +21,7 @@
 # return  a list of the allele frequeny at each time point
 # number of generations of fixation is the length of your list
 
+#exercise 1:
 import numpy as np 
 import matplotlib.pyplot as plt
 
@@ -37,14 +38,58 @@ def wfmodel(pop, afreq):
 
 # print(wfmodel(popsize, frequency))
 
+#exercise 2: 
 gen_num = []
 for i in range(1000):
 	my_output = wfmodel(popsize, frequency)
 	gen_num.append(len(my_output))
 
 
-fig, ax = plt.subplots()
+fig, ax, ax1, ax2 = plt.subplots()
 
-ax.hist(gen_num)
+#ax.hist(gen_num)
+# plt.show()
+
+#exercise 3:
+
+# pick 5 population sizes of at least 50
+pop_sizes = [75, 500, 10000, 32500, 50000]
+
+# for each pop size run the model 50 times
+avgs = []
+gen_num = []
+for i in range(len(pop_sizes)):
+	for outs in range(50):
+		my_output = wfmodel(pop_sizes[i], frequency) # allele frequency is still 0.5
+		gen_num.append(len(my_output))
+	avgs.append(np.mean(gen_num))
+
+# create a scatter polt
+#	x = pop size , y = average time to fixation
+ax1.scatter(pop_sizes, avgs, c = "black")
+ax1.set_xlabel("Population Size")
+ax1.set_ylabel("Average Generation Time to Fixation")
+ax1.set_title("Multiple Population Size Model")
 plt.show()
+
+# pick 5 different allele frequencies
+allele_freqs = [0.1, 0.27, 0.5, 0. 78, 0.9]
+
+
+alavgs = []
+gen_num2 = []
+for i in range(len(allele_freqs)):
+	for outs in range(10):
+		my_output = wfmodel(pop_sizes[i], frequency) # allele frequency is still 0.5
+		gen_num2.append(len(my_output))
+	alavgs.append(np.mean(gen_num2))
+
+
+fig2, ax2 = plt.subplots()
+ax2.scatter(allele_freqs, alavgs, c = "black")
+ax2.set_xlabel("Allele Frequencies")
+ax2.set_ylabel("Average Generation Time to Fixation")
+ax2.set_title("Multiple Allele Frequency Model")
+plt.show()
+
 
