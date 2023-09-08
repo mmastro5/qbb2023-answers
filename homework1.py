9b6e4fe0ccb7890a011b69f69ae055cd08ad8820
Maya <mmastro5@jhu.edu>
2023-09-08 15:58:22 -0400

    Homework 1

---
+++ homework1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+#importing essential libraries
+import numpy
+
+#opening file
+f = open(inflammation-01.csv, "r")
+lines = f.readlines()
+patient5 = []
+
+# for line in lines[]:
+# 	patient5 = 
+
+
+
+print(lines)
+
+# exercise 1
+
+
